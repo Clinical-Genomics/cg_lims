5d74bc0c7b7681855a08c4d81b6c9568b753a9aa
mayabrandi <maya.brandi@scilifelab.se>
2020-10-01 10:26:46 +0200

    Update move_samples.py

--- cg_lims/EPPs/move/move_samples.py
+++ cg_lims/EPPs/move/move_samples.py
@@ -16,11 +16,11 @@ import click
 @options.workflow_id(help="Destination workflow id.")
 @options.stage_id(help="Destination stage id.")
 @options.udf(help="UDF that will tell wich artifacts to move.")
-@options.input_artifacts(
+@options.input(
     help="Use this flag if you want to queue the input artifacts of the current process. Default is to queue the output artifacts (analytes) of the process."
 )
 @click.pass_context
-def move_samples(ctx, workflow_id: str, stage_id: str, udf: str, input_artifacts:bool):
+def move_samples(ctx, workflow_id: str, stage_id: str, udf: str, input:bool):
     """Script to move aritfats to another stage.
     
     Queueing artifacts with <udf==True>, to stage with <stage-id>
@@ -29,7 +29,7 @@ def move_samples(ctx, workflow_id: str, stage_id: str, udf: str, input_artifacts
     process = ctx.obj["process"]
     lims = ctx.obj["lims"]
 
-    artifacts = get_artifacts(process=process, input=input_artifacts)
+    artifacts = get_artifacts(process=process, input=input)
     filtered_artifacts = filter_artifacts(artifacts, udf, True)
 
     try:
