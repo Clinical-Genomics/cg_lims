d0e92929892f74a6f3d19e504fbeb36689104709
mayabrandi <maya.brandi@scilifelab.se>
2020-10-02 13:50:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 
 from setuptools import setup, find_packages
 
+try:
+    with open("requirements.txt", "r") as f:
+        install_requires = [x.strip() for x in f.readlines()]
+except IOError:
+    install_requires = []
+
+
 setup(name='cg_lims',
         version='1.0',
         description='',
