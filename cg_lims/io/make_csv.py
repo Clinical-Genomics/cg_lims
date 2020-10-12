226237ea75ae0142e4ca999f64dae9fc01b9abb7
Maya <maya.brandi@scilifelab.se>
2020-10-12 11:26:36 +0200

    format

--- cg_lims/io/make_csv.py
+++ cg_lims/io/make_csv.py
@@ -2,7 +2,7 @@ import string
 import csv
 
 
-def make_plate_file(file: str, rows: dict, headers: list, col_range: int=13, row_range: int=8):
+def make_plate_file(file: str, rows: dict, headers: list, col_range: int=13, row_range: int=8, newline='\n', delimiter=','):
     """Creating a plate file.
     
     Arguments:
@@ -12,8 +12,8 @@ def make_plate_file(file: str, rows: dict, headers: list, col_range: int=13, row
             keys: plate position: eg A1, B1, E4, H2...
             values: list of file rows. Same length as the headers list."""
 
-    with open(file, "w") as hamilton_csv:
-        wr = csv.writer(hamilton_csv)  #format??
+    with open(file, "w", newline=newline) as hamilton_csv:
+        wr = csv.writer(hamilton_csv, delimiter=delimiter)
         wr.writerow(headers)
         for col in range(1, col_range):
             for row in string.ascii_uppercase[0:row_range]:
