310d751cc6c0946b87ce1d41782df859e5fbdf54
mayabrandi <maya.brandi@scilifelab.se>
2020-10-16 11:04:36 +0200

    Update files.py

--- cg_lims/get/files.py
+++ cg_lims/get/files.py
@@ -25,8 +25,8 @@ def get_lims_log_file(lims: Lims, file_id: str) -> pathlib.Path:
         server_adress = BASEURI.split(":")[1]
         file_path = files[0].content_location.split(server_adress)[1]
         file = pathlib.Path(file_path)
-        file.chmod(0o775)
     else:
         file = pathlib.Path(file_id)
+        file.chmod(0o775)
 
     return file
