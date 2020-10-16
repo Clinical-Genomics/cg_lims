7cc46fec6f3bfd41a1f31839fc7b65be407ada4d
mayabrandi <maya.brandi@scilifelab.se>
2020-10-16 07:52:33 +0200

    making it work

--- cg_lims/EPPs/udf/calculate/twist_pool.py
+++ cg_lims/EPPs/udf/calculate/twist_pool.py
@@ -1,11 +1,22 @@
 
 from cg_lims.objects import Pool
+from cg_lims.exceptions import LimsError, LowAmountError
+from cg_lims.get.artifacts import get_artifacts
 
+from genologics.entities import Artifact
+
+import logging
+import sys
 import click
 
+LOG = logging.getLogger(__name__)
+
+
 class TwistPool(Pool):
-    def __init__(self):
-        self.pool.qc_flag='PASSED'
+    
+    def __init__(self, pool_artifact: Artifact):
+        super().__init__(pool_artifact)
+        self.pool.qc_flag = 'PASSED'
         self.total_volume = 0
         self.amount_fail = False
 
@@ -22,19 +33,19 @@ class TwistPool(Pool):
         for art in self.artifacts:
             reads = art.samples[0].udf.get('Reads missing (M)')
             concentration = art.udf.get('Concentration')
-            fract_of_pool = reads/float(self.total_reads)
-            amount_to_pool = self.pool.udf.get('Total Amount (ng)') * fract_of_pool
+            fract_of_pool = reads/float(self.total_reads_missing)
+            amount_to_pool = self.pool.udf.get(
+                'Total Amount (ng)') * fract_of_pool
             vol = amount_to_pool/concentration
             if vol > 15:
                 vol = 15
             if amount_to_pool > art.udf.get('Amount (ng)') or amount_to_pool < 187.5:
-                self.pool.qc_flag='FAILED'
+                self.pool.qc_flag = 'FAILED'
                 self.amount_fail = True
             art.udf['Amount taken (ng)'] = amount_to_pool
             art.udf['Volume of sample (ul)'] = vol
             art.put()
             self.total_volume += vol
-
 
 
 def calculate_volumes_for_pooling(pools):
@@ -43,28 +54,28 @@ def calculate_volumes_for_pooling(pools):
     Wahter is added to each pool, for them to all get the same volume."""
 
     amount_failed_for_some_pool = False
-    all_volumes= []
+    all_volumes = []
 
     for pool_art in pools:
-        pool = TwistPool(pool_artifact = pool_art)
-        pool.get_total_reads()
+        pool = TwistPool(pool_art)
+        pool.get_total_reads_missing()
         pool.calculate_amount_and_volume()
         if pool.amount_fail:
             amount_failed_for_some_pool = True
         if pool.total_volume:
-            pool_art['Total Volume (ul)'] = pool.total_volume
+            pool_art.udf['Total Volume (ul)'] = pool.total_volume
         all_volumes.append(pool.total_volume)
 
     for pool_art in pools:
         if pool_art.udf.get('Total Volume (ul)'):
-            pool_art.udf['Volume H2O (ul)'] = max(all_volumes) - pool_art.udf.get('Total Volume (ul)')
+            pool_art.udf['Volume H2O (ul)'] = max(
+                all_volumes) - pool_art.udf.get('Total Volume (ul)')
         pool_art.put()
-    
+
     if amount_failed_for_some_pool:
         raise LowAmountError(
             'Input amount low for samples in some pool. Generate placement map for more info.'
         )
-
 
 
 @click.command()
@@ -73,11 +84,9 @@ def twist_pool(ctx):
     """Calculates volumes for pools in a plate before hybridization."""
 
     process = ctx.obj["process"]
-    lims = ctx.obj["lims"]
 
     try:
         pools = get_artifacts(process=process, input=False)
         calculate_volumes_for_pooling(pools)
     except LimsError as e:
         sys.exit(e.message)
-
--- cg_lims/objects.py
+++ cg_lims/objects.py
@@ -1,8 +1,14 @@
-from cg_lims.exceptions import ZeroReadsError
+  
+"""General Lims Objects ??? Not sure what to write
+"""
+
+from cg_lims.exceptions import ZeroReadsError, MissingUDFsError
 from genologics.entities import Artifact
 
 
 class Pool:
+    """Collecting informatiuon general to any pool"""
+
     def __init__(self, pool_artifact: Artifact):
         self.total_reads_missing = 0
         self.pool = pool_artifact
@@ -14,11 +20,11 @@ class Pool:
         for art in self.artifacts:
             reads = art.samples[0].udf.get('Reads missing (M)')
             if reads is None:
-                sys.exit('Missing udfs: Reads missing (M)')
+                raise MissingUDFsError('Missing udfs: Reads missing (M)')
             self.total_reads_missing += reads
         if self.total_reads_missing == 0:
             raise ZeroReadsError(
-            "All samples seem to have Missing Reads = 0. You dont want to sequence any of the samples?"
+            "All samples seem to have Missing Reads = 0!"
         )
 
 
