44f64a473023ed7441e349720747d342fb9aaf7f
mayabrandi <maya.brandi@scilifelab.se>
2020-10-22 10:45:16 +0200

    Update files.py

--- cg_lims/get/files.py
+++ cg_lims/get/files.py
@@ -1,17 +1,15 @@
-from genologics.entities import Process, Artifact, Sample
+from genologics.entities import Artifact
 from genologics.config import BASEURI
 from genologics.lims import Lims
 
-from operator import attrgetter
-import sys
 import pathlib
 from typing import List
 
 
 def get_log_content(lims: Lims, file_id: str) -> str:
-    """Searching for a log Artifact with file_id. 
+    """Searching for a log file Artifact with file_id. 
     
-    If the artifact is found, returning the the file content."""
+    If the artifact is found, returning its file content."""
 
     log_artifact = Artifact(lims, id=file_id)
 
@@ -23,9 +21,9 @@ def get_log_content(lims: Lims, file_id: str) -> str:
     if files:
         server_adress = BASEURI.split(":")[1]
         file_path = files[0].content_location.split(server_adress)[1]
-        old_log = pathlib.Path(file_path)
-        old_log_content = old_log.read_text()
+        log_file = pathlib.Path(file_path)
+        log_file_content = log_file.read_text()
     else:
-        old_log_content = ''
+        log_file_content = ''
 
-    return old_log_content
+    return log_file_content
