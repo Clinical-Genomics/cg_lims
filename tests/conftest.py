c29c09a8b413cce46a2ed9f6389e192318d92b65
mayabrandi <maya.brandi@scilifelab.se>
2020-10-27 12:52:13 +0100

    adding helpers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
 import pytest
 from genologics_mock.lims import MockLims
-from genologics_mock.entities import MockArtifact, MockProcess, MockProcessType, MockSample
-
+from genologics_mock.entities import (
+    MockArtifact,
+    MockProcess,
+    MockProcessType,
+    MockSample,
+)
+from .helpers import Helpers
 
 
 @pytest.fixture
@@ -17,13 +22,21 @@ def lims():
 
 
 @pytest.fixture
-def lims_with_data():
-    process_type = MockProcessType(name='SomeTypeOfProcess')
-    process_1 = MockProcess(process_type=process_type, date_run='2019-01-01')
-    process_2 = MockProcess(process_type=process_type, date_run='2020-01-01')
-    sample = MockSample(sample_id='some_sample_id')
-    artifact1 = MockArtifact(samples=[sample], parent_process=process_1, type='Analyte')
-    artifact2 = MockArtifact(samples=[sample], parent_process=process_2, type= 'Analyte')
-    lims = MockLims()
-    lims.artifacts = [artifact1, artifact2]
-    return lims
+def sample():
+    return MockSample()
+
+
+@pytest.fixture
+def artifact():
+    return MockArtifact()
+
+
+@pytest.fixture
+def process():
+    return MockProcess()
+
+
+@pytest.fixture(name="helpers")
+def fixture_helpers():
+    """Return a class with small helper functions"""
+    return Helpers()
