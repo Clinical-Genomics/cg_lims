87b47ec6a8544268f812394ab1cc3a5c5d062822
mayabrandi <maya.brandi@scilifelab.se>
2021-01-28 11:54:03 +0100

    typehints

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,7 @@ from limsmock.server import run_server
 PORT = 8000
 HOST = '127.0.0.1'
 
+
 @pytest.fixture
 def server_flat_tests():
     """Fixture for simple tests that don't assume specific nested relations between entities.
@@ -56,8 +57,9 @@ def server_make_kapa_csv_missing_udfs():
     thread.start()
     time.sleep(0.1)
 
+
 @pytest.fixture
-def lims():
+def lims() -> Lims:
     """Get genologics lims instance"""
 
     lims = Lims(f"http://{HOST}:{PORT}", 'dummy', 'dummy')
@@ -65,7 +67,7 @@ def lims():
 
 
 @pytest.fixture
-def artifact_1(lims):
+def artifact_1(lims) -> Artifact:
     """Basic artifact with id 1. Containing no udfs.
     Related to sample_1."""
 
@@ -75,7 +77,7 @@ def artifact_1(lims):
 
 
 @pytest.fixture
-def sample_1(lims):
+def sample_1(lims) -> Sample:
     """Basic sample with id S1. Containing no udfs.
     Related to artifact_1."""
 
@@ -85,7 +87,7 @@ def sample_1(lims):
 
 
 @pytest.fixture
-def artifact_2(lims):
+def artifact_2(lims) -> Artifact:
     """Basic artifact with id 2. Containing no udfs.
     Related to sample_2."""
 
@@ -95,7 +97,7 @@ def artifact_2(lims):
 
 
 @pytest.fixture
-def sample_2(lims):
+def sample_2(lims) -> Sample:
     """Basic sample with id S2. Containing no udfs.
     Related to artifact_2."""
 
@@ -103,41 +105,43 @@ def sample_2(lims):
 
     return sample
 
+
 @pytest.fixture
-def config():
+def config() -> str:
     """Get file path to config"""
 
     return "tests/fixtures/config.yaml"
 
 
 @pytest.fixture
-def enzymatic_fragmentation_file():
+def enzymatic_fragmentation_file() -> str:
     """Get file path to valid json"""
+
     file_path = "tests/fixtures/Enzymatic_fragmentation"
     file = Path(file_path)
     return file.read_text()
 
 
 @pytest.fixture
-def kapa_library_preparation_file():
+def kapa_library_preparation_file() -> str:
     """Get file path to valid json"""
+
     file_path = "tests/fixtures/KAPA_Library_Preparation"
     file = Path(file_path)
     return file.read_text()
 
 
 @pytest.fixture
-def kapa_library_preparation_file_missing_udf():
+def kapa_library_preparation_file_missing_udf() -> str:
     """Get file path to valid json"""
+
     file_path = "tests/fixtures/KAPA_Library_Preparation_missing_udf"
     file = Path(file_path)
     return file.read_text()
 
 
 @pytest.fixture(name="cli_runner")
-def fixture_cli_runner():
+def fixture_cli_runner() -> CliRunner:
     """Create a CliRunner"""
     runner = CliRunner()
     return runner
-
-
