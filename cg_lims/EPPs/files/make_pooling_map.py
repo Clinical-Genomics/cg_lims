b194a9315cec66edc5597a2444b0b2793ff05149
mayabrandi <maya.brandi@scilifelab.se>
2021-03-18 12:17:26 +0100

    wip

---
+++ cg_lims/EPPs/files/make_pooling_map.py
@@ -0,0 +1,158 @@
+import logging
+import sys
+from typing import List
+from pathlib import Path
+
+import click
+from genologics.entities import Artifact, Process
+
+from cg_lims.exceptions import LimsError, LowAmountError
+from cg_lims.get.artifacts import get_artifacts
+from cg_lims.objects import Pool
+
+LOG = logging.getLogger(__name__)
+
+
+# The avalible volume of a sample is allways 15.
+AVALIBLE_SAMPLE_VOLUME = 15
+
+# A sample should not have les than 187.5 ng in the pool
+MINIMUM_SAMPLE_AMOUNT = 187.5
+
+
+def add_pool_info(pool_udfs:List[str], pool: Artifact):
+    html = []
+    for udf in pool_udfs:
+        value = pool.udf.get(udf)
+        if value is not None:
+            html.append(
+                f'<tr><td class="group-field-label">{udf}: </td><td class="group-field-value"></td></tr>'
+            )
+            html.append(str(value))
+    return ''.join(html)
+
+
+def add_sample_info_headers(udfs: List[str]):
+    html = []
+    for udf in udfs:
+        html.append(f'<th style="width: 7%;" class="">{udf}</th>')
+    return ''.join(html)
+
+def add_sample_info(sample: Artifact, sample_udfs: List[str]):
+    html = []
+    return ''.join(html)
+
+
+    < th
+    style = "width: 7%;"
+
+    class ="" > Total amount of sample < / th >
+
+    < th
+    style = "width: 7%;"
+
+    class ="" > Volume of Sample < / th >
+
+def make_html(
+    resultfile: Path,
+    pools: List[Artifact],
+    process: Process,
+    pool_udfs: List[str],
+    sample_udfs: List[str],
+):
+    ### HEADER ###
+    html = []
+    html.append(
+        f"""
+        <html>
+        <head>
+        <style>table, th, td {{border: 1px solid black; border-collapse: collapse;}}</style>
+        <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+        <link href="../css/g/queue-print.css" rel="stylesheet" type="text/css" media="screen,print">
+        <title>{process.type.name}</title>
+        </head>
+        <body>
+        <div id="header">
+        <h1 class="title">{process.type.name}</h1>
+        </div>
+        Created by: {USERNAME}, {date.today().isoformat()}""")
+
+    ### POOLS ###
+    for pool in pools:
+        artifacts = [(a.location[1], a) for a in pool.input_artifact_list()]
+        # sorting on columns
+        artifacts.sort(key=lambda tup: tup[0][1])
+        nr_samples = len(artifacts)
+
+        # Information about the pool
+        html.append(
+            f"""<table class="group-contents">
+            <thead>
+            <tr><th class="group-header" colspan="10"><h2> {pool.name}</h2>
+            <table>
+            <tbody>
+            <tr>
+            <td class="group-field-label">Container LIMS ID: </td>
+            <td class="group-field-value">{pool.id}</td>
+            </tr>
+            <tr>
+            <td class="group-field-label">Nr samples in pool: </td>
+            <td class="group-field-value">{nr_samples}</td>
+            </tr>
+            {add_pool_info(pool_udfs, pool)}
+            </tbody>
+            </table>
+            <br>
+            """
+        )
+
+        # Information about samples the pool
+        ## Columns Header
+        html.append(
+            f"""<tr>
+                <th style="width: 7%;" class="">Sample Lims ID</th>
+                <th style="width: 7%;" class="">Source Well</th>
+                <th style="width: 7%;" class="">Source Container</th>
+                <th style="width: 7%;" class="">Pool Name</th>
+                {add_sample_info_headers(sample_udfs)}
+                </tr>"""
+        )
+        html.append(
+            """</thead>
+            <tbody>""")
+
+        ## artifact list
+        for location, art in artifacts:
+            sample = art.samples[0]
+
+            html.append(f"""
+            <tr>
+            <td style="width: 7%;">{sample.id}</td>
+            <td class="" style="width: 7%;">{location}</td>
+            <td class="" style="width: 7%;">{art.container.name}</td>
+            <td class="" style="width: 7%;">{pool.name}</td>
+            {add_sample_info(sample, sample_udfs)}
+            </tr>
+            </tbody>
+            </table>
+            <br><br>
+            </html>""")
+
+
+    file = open(str(resultfile) + ".html", "w")
+    file.write("".join(html).encode("utf-8"))
+    file.close()
+
+
+@click.command()
+@click.pass_context
+def twist_pool(ctx):
+    """Calculates volumes for pools in a plate before hybridization."""
+
+    LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
+    process = ctx.obj["process"]
+    try:
+        pools = get_artifacts(process=process, input=False)
+        make_html(pools)
+    except LimsError as e:
+        sys.exit(e.message)
