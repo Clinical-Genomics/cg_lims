fd2a741bc8a5e56c0754135089581869c59a6db0
BaseSpace LIMS - Automation Worker user <glsai@clinical-lims-stage.scilifelab.se>
2021-03-30 11:48:16 +0200

    quick fix.

--- cg_lims/EPPs/files/file_to_udf.py
+++ cg_lims/EPPs/files/file_to_udf.py
@@ -45,11 +45,16 @@ def set_udfs(well_field: str, value_field: str, udf: str, well_dict: dict, resul
         for sample in reader:
             well = sample.get(well_field)
             value = sample.get(value_field)
+            print(type(value))
+            print(value)
             if value is None or well not in well_dict:
                 error_msg = "Some samples in the step were not represented in the file."
                 continue
             art = well_dict[well]
-            art.udf[udf] = value
+            try:
+            	art.udf[udf] = str(value)
+            except:
+                art.udf[udf] = float(value)
             art.put()
             passed_arts += 1
 
