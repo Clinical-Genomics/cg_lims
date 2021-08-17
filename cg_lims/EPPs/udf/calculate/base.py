9b7e366538571e4bcc187cdd8b55640d4858ebb8
mayabrandi <maya.brandi@scilifelab.se>
2021-08-17 14:38:03 +0200

    Pool reads (#79) (Minor)
    ### Added
    - script to sum missing reads for pool

--- cg_lims/EPPs/udf/calculate/base.py
+++ cg_lims/EPPs/udf/calculate/base.py
@@ -7,7 +7,7 @@ from cg_lims.EPPs.udf.calculate.twist_aliquot_volume import twist_aliquot_volume
 from cg_lims.EPPs.udf.calculate.twist_get_volumes_from_buffer import get_volumes_from_buffer
 from cg_lims.EPPs.udf.calculate.calculate_water import volume_water
 from cg_lims.EPPs.udf.calculate.molar_concentration import molar_concentration
-
+from cg_lims.EPPs.udf.calculate.sum_missing_reads_in_pool import missing_reads_in_pool
 
 # commands
 from cg_lims.EPPs.udf.calculate.twist_pool import twist_pool
@@ -32,3 +32,4 @@ calculate.add_command(get_missing_reads)
 calculate.add_command(calculate_amount_ng)
 calculate.add_command(volume_water)
 calculate.add_command(molar_concentration)
+calculate.add_command(missing_reads_in_pool)
