7bf4f5cc750f9417dadb0af94974ea24432cd4fe
mayabrandi <maya.brandi@scilifelab.se>
2021-09-17 13:47:23 +0200

    correcting dock string

--- cg_lims/EPPs/mongo/base.py
+++ cg_lims/EPPs/mongo/base.py
@@ -8,7 +8,7 @@ from cg_lims.EPPs.mongo.prep_microbial import microbial_prep_document
 @click.group(invoke_without_command=True)
 @click.pass_context
 def mongo(ctx):
-    """Main entry point of calculate commands"""
+    """Main load commands."""
     pass
 
 
