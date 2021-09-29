38552934843f209904f3a60ac6cd44208d53c79c
Cecilia Svensson <37171914+cesve@users.noreply.github.com>
2021-09-29 15:09:10 +0200

    Update WGS.py

--- cg_lims/models/database/prep/WGS.py
+++ cg_lims/models/database/prep/WGS.py
@@ -4,20 +4,37 @@ from pydantic import Field
 
 from cg_lims.objects import BaseAnalyte
 
+""Från protocol Initial QC wgs v4:""
+class InitialQCwgsv4ArtifactUDF(BaseModel):
+    sample_concentration: float = Field(..., alias="Concentration")
+    sample_amount: float = Field(..., alias="Amount (ng)")
+    sample_priority: float = Field(..., alias="priority")
 
-class LibraryPrepNexteraProcessUDFS(BaseModel):
-    lot_nr_tagmentation_buffer: str = Field(..., alias="Lot nr: Tagmentation buffer (TD-buffer)")
-    lot_nr_tagmentation_enzyme: str = Field(..., alias="Lot nr: Tagmentation enzyme (TDE1)")
-    lot_nr_index: str = Field(..., alias="Lot nr: Index")
-    lot_nr_pcr_mix: str = Field(..., alias="Lot nr: KAPA HiFi HotStart ReadyMix (2X)")
-    pcr_instrument_incubation: str = Field(..., alias="PCR instrument incubation")
-    pcr_instrument_amplification: str = Field(..., alias="PCR instrument amplification")
-    nr_pcr_cycles: int = Field(..., alias="Nr PCR cycles")
-
-
-
-class BufferExchangeProcessUDFS(BaseModel):
-    lot_nr_beads_buffer_exchange: Optional[str] = Field(None, alias="Lot Nr: Purification Beads")
-    lot_nr_etoh_buffer_exchange: Optional[str] = Field(None, alias="Lot Nr: EtOH")
-    lot_nr_h2o_buffer_exchange: Optional[str] = Field(None, alias="Lot Nr: Nuclease-free water")
-    buffer_exchange_method: Optional[str] = Field(None, alias="Method document")
+        
+class AliquotSamplesforCovarisArtifactUDF(BaseModel):        
+    sample_amount_needed: float = Field(..., alias="Amount needed (ng)")
+        
+        
+class FragmentDNATruSeqDNAProcessUDFS(BaseModel):        
+    lot_nr_covaris_tube: str = Field(..., alias="Lot no: Covaris tube")
+    fragmentation_method: str = Field(..., alias="Method Document 1")
+    covaris_protocol: str = Field(..., alias="Protocol")
+    lot_nr_resuspension_buffer_fragmentation: str = Field(..., alias="Lot no: Resuspension Buffer")
+        
+        
+class EndrepairSizeselectionA_tailingandAdapterligationTruSeqPCR_freeDNAProcessUDFS(BaseModel):
+    lot_nr_truseq_library_preparation_kit: Optional[float] = Field(None, alias="Lot no: TruSeq DNA PCR-Free Sample Prep Kit")
+    lot_nr_index: str = Field(..., alias="Lot no: Adaptor Plate")
+    lot_nr_beads: str = Field(..., alias="Lot no: SP Beads")
+    lot_nr_lucigen_library_preparation_kit: Optional[float] = Field(None, alias="Lot no: Lucigen prep kit")
+    pcr_instrument_incubation: str = Field(..., alias="PCR machine")
+    lot_nr_h2o_library_preparation: str = Field(..., alias="Lot no: Nuclease free water")
+    lot_nr_resuspension_buffer_library_preparation: str = Field(..., alias="Lot no: Resuspension buffer")
+    library_preparation_method: str = Field(..., alias="Method document")
+    lot_nr_etoh_library_preparation: str = Field(..., alias="Ethanol lot")
+        
+class EndrepairSizeselectionA_tailingandAdapterligationTruSeqPCR_freeDNAArtifactUDF(BaseModel):
+    finished_library_concentration: Optional[float] = Field(None, alias="Concentration")
+    finished_library_concentration_nm: float = Field(..., alias="Concentration (nM)")
+    finished_library_size: Optional[float] = Field(None, alias="Size (bp)")
+    
