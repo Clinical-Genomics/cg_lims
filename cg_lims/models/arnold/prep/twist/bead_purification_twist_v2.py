2fd345fc40aa83e9e51e78475d1a561d5c0523dc
Cecilia Svensson <37171914+cesve@users.noreply.github.com>
2021-09-30 15:19:10 +0200

    Update bead_purification_twist_v2.py

--- cg_lims/models/arnold/prep/twist/bead_purification_twist_v2.py
+++ cg_lims/models/arnold/prep/twist/bead_purification_twist_v2.py
@@ -4,13 +4,24 @@ from cg_lims.objects import BaseAnalyte
 
 
 class BeadPurificationArtifactUDFs(BaseModel):
-    size_bp: Optional[str] = Field(None, alias="Size (bp)")
-    concentration: Optional[str] = Field(None, alias="Concentration")
+    size_bp: str = Field(..., alias="Size (bp)")
+    concentration: str = Field(..., alias="Concentration")
+        
+        
+class BeadPurificationposthybTwistProcessUDFs(BaseModel):
+    lot_nr_enrichment_kit_hybridization: str = Field(..., alias="Twist Enrichment Kit")
+    lot_nr_etoh_bead_purification_post_hyb: str = Field(..., alias="Ethanol")
+    lot_nr_h2o_bead_purification_post_hyb: str = Field(..., alias="Nuclease free water")
+    bead_purification_post_hyb_method: str = Field(..., alias="Method document")
+
 
 
 class BeadPurificationUDFs(BeadPurificationArtifactUDFs):
     class Config:
         allow_population_by_field_name = True
+        
+        
+      
 
 
 def get_bead_purification_twist(lims: Lims, sample_id: str) -> BeadPurificationUDFs:
