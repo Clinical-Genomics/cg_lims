8b46b19960a61629494799358bbf768943d7c7de
Cecilia Svensson <37171914+cesve@users.noreply.github.com>
2021-09-30 13:56:42 +0200

    Update enzymatic_fragmentation_twist.py

--- cg_lims/models/arnold/prep/twist/enzymatic_fragmentation_twist.py
+++ cg_lims/models/arnold/prep/twist/enzymatic_fragmentation_twist.py
@@ -1,1 +1,16 @@
+from typing import Optional
 
+from genologics.lims import Lims
+from pydantic import Field, BaseModel
+
+from cg_lims.objects import BaseAnalyte
+
+
+class enzymaticfragmentationTWISTv2ProcessUdfs(BaseModel):
+    enzymatic_fragmentation_method: Optional[str] = Field(None, alias="Method document")
+    fragmentation_time: Optional[str] = Field(None, alias="Fragmentation time (min)")
+    pcr_instrument_fragmentation: Optional[str] = Field(None, alias="Thermal cycler name")
+    library_preparation_kit_fragmentation: Optional[str] = Field(None, alias="KAPA HyperPlus Kit")
+   
+      
+""Kan även vara bra att ha well position och container name""
