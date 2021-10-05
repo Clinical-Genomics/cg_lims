7c5a9a0cd36fe978da50b067637547acea4403e5
Cecilia Svensson <37171914+cesve@users.noreply.github.com>
2021-10-05 11:46:06 +0200

    Update amplify_captured_libraries.py

--- cg_lims/models/arnold/prep/twist/amplify_captured_libraries.py
+++ cg_lims/models/arnold/prep/twist/amplify_captured_libraries.py
@@ -12,3 +12,5 @@ class AmplifycapturedlibrariestwistProcessUDFs(BaseModel):
     lot_nr_amplification_kit_amplify_captured_library: str = Field(..., alias="Kapa HiFi HotStart ReadyMix")
     nr_pcr_cycles_amplify_captured_library: str = Field(..., alias="Nr of PCR cycles")
    
+# well position (optional)
+# container name (optional)
