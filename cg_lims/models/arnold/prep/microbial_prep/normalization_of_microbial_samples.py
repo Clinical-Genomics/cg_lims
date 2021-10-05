093138f0bbf224e0961683c8602e0ab8a809fff5
Cecilia Svensson <37171914+cesve@users.noreply.github.com>
2021-10-05 11:32:30 +0200

    Update normalization_of_microbial_samples.py

--- cg_lims/models/arnold/prep/microbial_prep/normalization_of_microbial_samples.py
+++ cg_lims/models/arnold/prep/microbial_prep/normalization_of_microbial_samples.py
@@ -15,6 +15,9 @@ class NormalizationOfMicrobialSamplesProcessUDFS(BaseModel):
     lot_nr_dilution_buffer_sample_normalization: Optional[str] = Field(
         None, alias="Dilution buffer lot no"
     )
+        
+# well position 
+# container name
 
 
 class NormalizationOfMicrobialSamplesUDFS(NormalizationOfMicrobialSamplesProcessUDFS):
