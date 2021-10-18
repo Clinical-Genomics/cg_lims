7a34211d9f7c40d5dc7c6e5df1800a1b44ef71e1
barrystokman <barry.stokman@gmail.com>
2021-10-18 14:30:28 +0200

    nearly there

--- tests/EPPs/udf/calculate/test_calculate_resuspension_buffer_volumes.py
+++ tests/EPPs/udf/calculate/test_calculate_resuspension_buffer_volumes.py
@@ -6,10 +6,11 @@ from cg_lims.EPPs.udf.calculate.calculate_resuspension_buffer_volumes import (
     AMOUNT_NEEDED_LUCIGEN,
     AMOUNT_NEEDED_TRUSEQ,
     VALID_AMOUNTS_NEEDED,
+    calculate_rb_volume,
     calculate_sample_rb_volume,
     pre_check_amount_needed_filled_correctly,
 )
-from cg_lims.exceptions import InvalidValueError
+from cg_lims.exceptions import InvalidValueError, MissingUDFsError
 
 
 def test_pre_check_amount_needed_filled_correctly(
@@ -61,3 +62,47 @@ def test_calculate_sample_rb_volume(sample_volume, total_volume, expected_return
 
     # THEN the correct value should be returned
     assert result == expected_return_value
+
+
+@mock.patch("cg_lims.get.fields.get_amount_needed")
+@mock.patch("cg_lims.get.fields.get_concentration")
+def test_calculate_rb_volume_missing_concentration(
+    mock_get_concentration, mock_get_amount_needed, artifact_1: Artifact
+):
+    # GIVEN a list of artifacts with one artifact missing the udf 'Concentration'
+    artifacts = [artifact_1]
+    mock_get_concentration.return_value = None
+
+    # WHEN calculating the rb volumes for all samples
+    with pytest.raises(MissingUDFsError) as error_message:
+        calculate_rb_volume(artifacts)
+
+    # THEN the MissingUDFsError exception should be raised
+    assert (
+        f"Could not apply calculation for 1 out of 1 sample(s): 'Concentration' is missing!"
+        in error_message.value.message
+    )
+
+
+@mock.patch(
+    "cg_lims.EPPs.udf.calculate.calculate_resuspension_buffer_volumes.get_amount_needed"
+)
+@mock.patch(
+    "cg_lims.EPPs.udf.calculate.calculate_resuspension_buffer_volumes.get_concentration"
+)
+def test_calculate_rb_volume_(
+    mock_get_concentration,
+    mock_get_amount_needed,
+    artifact_1: Artifact,
+):
+    # GIVEN a list of artifacts with one artifact
+    mock_get_concentration.return_value = 100
+    mock_get_amount_needed.return_value = 1100
+    artifacts = [artifact_1]
+
+    # WHEN calculating the rb volumes for all samples
+    calculate_rb_volume(artifacts)
+
+    # THEN the correct volumes should be set on the artifact
+    assert artifact_1.udf["RB Volume (ul)"] == 44.0
+    assert artifact_1.udf["Sample Volume (ul)"] == 11.0
