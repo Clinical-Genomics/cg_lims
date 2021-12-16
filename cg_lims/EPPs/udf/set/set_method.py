03f90b6b22a007e171b54928e4cceabca2910c50
mayabrandi <maya.brandi@scilifelab.se>
2021-12-16 11:15:53 +0100

    splitting into function

--- cg_lims/EPPs/udf/set/set_method.py
+++ cg_lims/EPPs/udf/set/set_method.py
@@ -2,7 +2,7 @@
 """
 import logging
 import sys
-from typing import List
+from typing import List, Optional
 
 import click
 
@@ -15,23 +15,40 @@ from cg_lims.exceptions import LimsError, MissingUDFsError
 LOG = logging.getLogger(__name__)
 
 
-def get_document_paths(document_udfs: List[str], process: Process, atlas_host: str) -> List[str]:
-    """Get method document paths from Atlas"""
-    method_documents = []
-    for document_udf in document_udfs:
-        document_title: str = process.udf.get(document_udf)
-        if not document_title:
-            LOG.warning(f"Process Udf: {document_udf} does not exist or was left empty.")
-            continue
-        response = requests.get(f"{atlas_host}/title/{document_title}/path")
-        if response.status_code != 200:
-            raise response  ##something
-        method_documents.append(response.json())
+def get_path(document_udf: str, process: Process, atlas_host: str) -> Optional[str]:
+    """Get method document path from Atlas"""
 
+    document_title: str = process.udf.get(document_udf)
+    if not document_title:
+        LOG.warning(f"Process Udf: {document_udf} does not exist or was left empty.")
+        return
+    response = requests.get(f"{atlas_host}/title/{document_title}/path")
+    if response.status_code != 200:
+        raise response  ##something
+    return response.json()
+
+
+def get_document_paths(document_udfs: List[str], process: Process, host: str) -> List[str]:
+    """Get method document paths from Atlas"""
+
+    method_documents: List[Optional[str]] = [get_path(udf, process, host) for udf in document_udfs]
+    return [doc for doc in method_documents if doc]
+
+
+def set_methods_and_version(document_udfs: List[str], process: Process, host: str) -> None:
+    """Set Method Documents and Atlas Version"""
+
+    method_documents = get_document_paths(document_udfs=document_udfs, host=host, process=process)
     if not method_documents:
         raise MissingUDFsError(message="Found no valid Method document UDFs in the step.")
-
-    return method_documents
+    version_response = requests.get(f"{host}/version")
+    if version_response.status_code != 200:
+        raise version_response  ##something
+    process.udf["Methods"] = " ,".join(method_documents)
+    process.udf["Atlas Version"] = version_response.json()
+    process.put()
+    LOG.info("Method Documents and Atlas Version have been set.")
+    click.echo("Method Documents and Atlas Version have been set.")
 
 
 @click.command()
@@ -43,16 +60,7 @@ def method_document(ctx: click.Context, process_udfs: List[str]):
     LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
     process: Process = ctx.obj["process"]
     atlas_host: str = ctx.obj["atlas_host"]
-
     try:
-        atlas_version = requests.get(f"{atlas_host}/version")
-        method_documents = get_document_paths(
-            document_udfs=process_udfs, atlas_host=atlas_host, process=process
-        )
-        process.udf["Methods"] = " ,".join(method_documents)
-        process.udf["Atlas Version"] = atlas_version.json()
-        process.put()
-        LOG.info("Method Documents and Atlas Version have been set.")
-        click.echo("Method Documents and Atlas Version have been set.")
+        set_methods_and_version(process=process, host=atlas_host, document_udfs=process_udfs)
     except LimsError as e:
         sys.exit(e.message)
--- tests/EPPs/udf/test_set_method.py
+++ tests/EPPs/udf/test_set_method.py
@@ -1,48 +1,95 @@
 import pytest
 from genologics.entities import Process
 
-from cg_lims.EPPs.udf.set.set_method import get_document_paths
-from datetime import datetime as dt
-
+from cg_lims.EPPs.udf.set.set_method import get_path, set_methods_and_version
+from cg_lims.exceptions import MissingUDFsError
+from fastapi import HTTPException
 from tests.conftest import (
     server,
 )
 
 import responses
 import requests
-from requests import Request
+import logging
 
 
 @pytest.fixture()
 @responses.activate
-def mock_responces():
-    document_title = "SomeTitle"
+def atlas_response_mock():
     atlas_document_path = "atlas/document/path.md"
-    atlas_host = "https://atlas/api"
-    url = f"{atlas_host}/{document_title}/path"
+    url = "https://atlas/api/SomeTitle/path"
     responses.add(responses.GET, url, json=atlas_document_path, status=200)
     return requests.get(url)
 
 
-def test_set_method(lims, mocker, mock_responces):
-    # GIVEN: ".
+def test_get_path(lims, mocker, atlas_response_mock):
+    # GIVEN: a lims with a process: id="24-196211"
+    # with a udf named "Method Document" with a value of some method document title in atlas: "SomeTitle"  and
+    # GIVEN: a atlas api with a endpoint "https://atlas/api/SomeTitle/path" returning "atlas/document/path.md"
 
     server("flat_tests")
-
-    document_title = "SomeTitle"
-    atlas_document_path = "atlas/document/path.md"
     document_udf = "Method Document"
     process = Process(lims=lims, id="24-196211")
-    process.udf[document_udf] = document_title
+    process.udf[document_udf] = "SomeTitle"
     process.put()
-
-    # WHEN running get_document_paths
     mocker.patch.object(requests, "get")
-    requests.get.return_value = mock_responces
+    requests.get.return_value = atlas_response_mock
 
-    document_paths = get_document_paths(
-        process=process, document_udfs=[document_udf], atlas_host="https://atlas/api"
+    # WHEN running get_path with the process, the atlas api host and the document udf
+    document_path = get_path(
+        process=process, document_udf=document_udf, atlas_host="https://atlas/api"
     )
 
-    # THEN assert the delivery date was set to todays date
-    assert document_paths == [atlas_document_path]
+    # THEN assert get_path returns the document path
+    assert document_path == "atlas/document/path.md"
+
+
+def test_get_path_missing_udf(lims, mocker, caplog):
+    # GIVEN: a lims with a process: id="24-196211" with no udf named "Method Document"
+
+    server("flat_tests")
+    document_udf = "Method Document"
+    process = Process(lims=lims, id="24-196211")
+    process.udf.clear()
+    mocker.patch.object(requests, "get")
+
+    # WHEN running get_path with the process, the atlas api host and the document udf
+    with caplog.at_level(logging.WARNING):
+        document_path = get_path(
+            process=process, document_udf=document_udf, atlas_host="https://atlas/api"
+        )
+
+    # THEN assert get_path returns the document path
+    assert f"Process Udf: {document_udf} does not exist or was left empty." in caplog.text
+    assert document_path is None
+
+
+@pytest.fixture()
+@responses.activate
+def atlas_response_mock_fail():
+    url = "https://atlas/api/SomeTitle/path"
+    responses.add(responses.GET, url, status=404)
+    return requests.get(url)
+
+
+def test_get_path_wrong_document_title(lims, mocker, atlas_response_mock_fail):
+    # GIVEN: a lims with a process: id="24-196211"
+    # with a udf named "Method Document" with a value of some method document title in atlas: "SomeTitle"  and
+    # GIVEN: a atlas api with a endpoint "https://atlas/api/SomeTitle/path" returning "atlas/document/path.md"
+
+    server("flat_tests")
+    document_udf = "Method Document"
+    process = Process(lims=lims, id="24-196211")
+    process.udf[document_udf] = "SomeWrongTitle"
+    process.put()
+    mocker.patch.object(requests, "get")
+    requests.get.return_value = atlas_response_mock_fail
+
+    # WHEN running get_path with the process, the atlas api host and the document udf
+    with pytest.raises(HTTPException) as error:
+        document_path = get_path(
+            process=process, document_udf=document_udf, atlas_host="https://atlas/api"
+        )
+
+    # THEN: assert
+    assert error.value
