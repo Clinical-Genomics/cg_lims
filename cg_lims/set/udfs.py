8b7db3aa2b27d832d0900a94b2f8f0ca2c143db5
mayabrandi <maya.brandi@scilifelab.se>
2022-02-24 15:04:34 +0100

    adding test

--- cg_lims/set/udfs.py
+++ cg_lims/set/udfs.py
@@ -7,14 +7,21 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-def copy_udf(destination_artifact: Artifact, source_artifact: Artifact, artifact_udfs: List[str]):
+def copy_udf(
+    destination_artifact: Artifact, source_artifact: Artifact, artifact_udfs: List[str]
+) -> None:
     """Copying artifact udfs from source artifact to destination artifact.
     Logging missing uds without raising error"""
+
+    artifacts_to_put = False
 
     for udf in artifact_udfs:
         if not source_artifact.udf.get(udf):
             message = f"artifact udf {udf} missing on artifact {source_artifact.id}"
             LOG.error(message)
             continue
-        destination_artifact.udf[udf] = source_artifact.udf.get(udf)
-    destination_artifact.put()
+        destination_artifact.udf[udf] = source_artifact.udf[udf]
+        artifacts_to_put = True
+
+    if artifacts_to_put:
+        destination_artifact.put()
