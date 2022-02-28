8bd806a669b4c25795bbd6f08e8f2c9382cfc04c
mayabrandi <maya.brandi@scilifelab.se>
2022-02-28 11:27:24 +0100

    epp to copy udf process to art

--- cg_lims/set/udfs.py
+++ cg_lims/set/udfs.py
@@ -1,8 +1,10 @@
 from typing import List
 
-from genologics.entities import Artifact
+from genologics.entities import Artifact, Process
 
 import logging
+
+from cg_lims.exceptions import MissingUDFsError
 
 LOG = logging.getLogger(__name__)
 
@@ -25,3 +27,22 @@ def copy_udf(
 
     if artifacts_to_put:
         destination_artifact.put()
+
+
+def copy_udf_process_to_artifact(
+    artifact: Artifact, process: Process, artifact_udf: str, process_udf: str
+) -> None:
+    """Copying process udf to artifact udf.
+    Logging and raising missing uds error"""
+
+    if not process.udf.get(process_udf):
+        message = f"process: {process.id} missing udf {process_udf}"
+        LOG.error(message)
+        raise MissingUDFsError(message=message)
+    try:
+        artifact.udf[artifact_udf] = process.udf[process_udf]
+        artifact.put()
+    except:
+        message = f"{artifact_udf} doesn't seem to be a valid artifact udf."
+        LOG.error(message)
+        raise MissingUDFsError(message=message)
