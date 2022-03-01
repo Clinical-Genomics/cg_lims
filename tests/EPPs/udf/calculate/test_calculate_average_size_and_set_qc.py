be57d1ff883a5be4aec865882a0db128d87be8bd
mayabrandi <maya.brandi@scilifelab.se>
2022-03-01 14:22:49 +0100

    adding two tests

--- tests/EPPs/udf/calculate/test_calculate_average_size_and_set_qc.py
+++ tests/EPPs/udf/calculate/test_calculate_average_size_and_set_qc.py
@@ -1,56 +1,39 @@
 import pytest
-from genologics.entities import Artifact
+from genologics.entities import Artifact, Process
 
-from cg_lims.exceptions import MissingUDFsError
+from cg_lims.EPPs.udf.calculate.calculate_average_size_and_set_qc import calculate_average_size
+from cg_lims.exceptions import MissingUDFsError, MissingCgFieldError
 from cg_lims.set.udfs import copy_artifact_to_artifact
 from tests.conftest import server
 
 
-def test_copy_udfs(lims):
-    # GIVEN a sample with a udf "Sequencing Analysis"
-    server("flat_tests")
-    concentration_udf = "Concentration (nM)"
-    volume_udf = "Volume (ul)"
-    source_artifact = Artifact(lims=lims, id="1")
-    destination_artifact = Artifact(lims=lims, id="2")
-    source_artifact.udf[concentration_udf] = 2
-    source_artifact.udf[volume_udf] = 3
-    source_artifact.put()
+def test_calculate_average_size(lims):
+    # GIVEN a list of artifacts where some have udf "size (bp)"
+    # set to some integer and some are None.
+    # And given that the average of the values is 244
 
-    # WHEN running copy_udf
-    copy_artifact_to_artifact(
-        source_artifact=source_artifact,
-        destination_artifact=destination_artifact,
-        artifact_udfs=[(concentration_udf, concentration_udf), (volume_udf, volume_udf)],
-    )
+    server("calculate_average_size_and_set_qc")
+    process = Process(lims, id="24-297378")
+    all_artifacts = process.all_inputs()
 
-    # THEN the udfs have been set:
-    assert destination_artifact.udf[volume_udf] == source_artifact.udf[volume_udf]
-    assert destination_artifact.udf[concentration_udf] == source_artifact.udf[concentration_udf]
+    # WHEN running calculate_average_size
+    average = calculate_average_size(all_artifacts=all_artifacts)
+
+    # THEN the calculated average is 244
+    assert int(average) == 244
 
 
-def test_copy_udfs_missing_udfs(lims, caplog):
-    # GIVEN a sample with a udf "Sequencing Analysis"
-    server("flat_tests")
-    concentration_udf = "Concentration (nM)"
-    volume_udf = "Volume (ul)"
-    source_artifact = Artifact(lims=lims, id="1")
-    source_artifact.udf.clear()
-    source_artifact.put()
-    destination_artifact = Artifact(lims=lims, id="2")
+def test_calculate_average_size_no_sizes_given(lims):
+    # GIVEN a list of artifacts that dont have the udf "size (bp)" set.
 
-    # WHEN running copy_udf
+    server("calculate_average_size_and_set_qc")
+    process = Process(lims, id="24-297378")
+    all_artifacts = process.all_inputs()
+    for artifact in all_artifacts:
+        artifact.udf.clear()
+        artifact.put()
+
+    # WHEN running calculate_average_size
+    # THEN MissingUDFsError is being raised
     with pytest.raises(MissingUDFsError) as error_message:
-        copy_artifact_to_artifact(
-            source_artifact=source_artifact,
-            destination_artifact=destination_artifact,
-            artifact_udfs=[(concentration_udf, concentration_udf), (volume_udf, volume_udf)],
-        )
-
-    # THEN the udfs have not been set
-    assert destination_artifact.udf.get(volume_udf) is None
-    assert destination_artifact.udf.get(concentration_udf) is None
-    assert f"Artifact udf {volume_udf} missing on artifact {source_artifact.id}" in caplog.text
-    assert (
-        f"Artifact udf {concentration_udf} missing on artifact {source_artifact.id}" in caplog.text
-    )
+        average = calculate_average_size(all_artifacts=all_artifacts)
