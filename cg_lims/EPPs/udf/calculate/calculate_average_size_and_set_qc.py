c9c634c9c8dd6701c5c9aeea7cd2ffba78a2f545
mayabrandi <maya.brandi@scilifelab.se>
2022-03-01 13:09:38 +0100

    some restructuring (#169)

--- cg_lims/EPPs/udf/calculate/calculate_average_size_and_set_qc.py
+++ cg_lims/EPPs/udf/calculate/calculate_average_size_and_set_qc.py
@@ -3,7 +3,7 @@ import sys
 import click
 import numpy as np
 
-from typing import List, Tuple
+from typing import List
 
 from genologics.entities import Artifact
 
@@ -14,48 +14,40 @@ from cg_lims.exceptions import LimsError, MissingUDFsError
 LOG = logging.getLogger(__name__)
 
 
-def calculate_average_size(all_artifacts: List[Artifact]) -> Tuple[float, int]:
+def calculate_average_size(all_artifacts: List[Artifact]) -> float:
     """Calculate average size (bp) from given list of artifacts.
-     Returns the average and number of missing UDF:s."""
-    size_list = []
-    missing_udf_count = 0
-    artifacts = []
-    for artifact in all_artifacts:
-        if not artifact.name[0:3] == 'NTC':
-            artifacts.append(artifact)
-    for artifact in artifacts:
-        try:
-            size_list.append(int(artifact.udf['Size (bp)']))
-        except:
-            missing_udf_count += 1
-            pass
+    Returns the average and number of missing UDF:s."""
+
+    artifacts = [artifact for artifact in all_artifacts if artifact.name[0:3] != "NTC"]
+
+    size_list = [
+        artifact.udf["Size (bp)"] for artifact in artifacts if artifact.udf.get("Size (bp)")
+    ]
+
     if not size_list:
-        sys.exit("Set 'Size (bp)' for at least one sample")
-    average = float(np.mean(size_list))
-    return average, missing_udf_count
+        raise MissingUDFsError("Udf missing for 'Size (bp)' sample(s).")
+    return float(np.mean(size_list))
 
 
-def set_average_and_qc(average: float, missing_udf_count: int,
-                       lower_threshold: str, upper_threshold: str,
-                       all_artifacts: List[Artifact]) -> None:
+def set_average_and_qc(
+    average: float,
+    lower_threshold: str,
+    upper_threshold: str,
+    all_artifacts: List[Artifact],
+) -> None:
     """Sets the given average to a list of artifacts.
-     Also assesses QC flags according to defined thresholds."""
+    Also assesses QC flags according to defined thresholds."""
+    qc_flag = "PASSED"
     if lower_threshold is not None and int(average) < int(lower_threshold):
-        qc_flag = 'FAILED'
+        qc_flag = "FAILED"
     elif upper_threshold and int(average) > int(upper_threshold):
-        qc_flag = 'FAILED'
-    else:
-        qc_flag = 'PASSED'
+        qc_flag = "FAILED"
+
     for artifact in all_artifacts:
-        artifact.udf['Average Size (bp)'] = str(average)
-        if artifact.qc_flag == 'PASSED':
+        artifact.udf["Average Size (bp)"] = str(average)
+        if artifact.qc_flag == "PASSED":
             artifact.qc_flag = qc_flag
         artifact.put()
-    if missing_udf_count:
-        raise MissingUDFsError(
-            f"Udf missing for {missing_udf_count} sample(s). "
-            f"Average size was calculated without taking account to them."
-        )
 
 
 @click.command()
@@ -64,20 +56,19 @@ def set_average_and_qc(average: float, missing_udf_count: int,
 @click.pass_context
 def calculate_average_size_and_set_qc(ctx, lower_threshold: str, upper_threshold: str) -> None:
     """Script to calculate and apply Average Size (bp) to a set of samples
-     given a subset of their real size lengths. NTC samples are ignored in the calculations."""
+    given a subset of their real size lengths. NTC samples are ignored in the calculations."""
 
     LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
     process = ctx.obj["process"]
 
     try:
-        artifacts = get_artifacts(process=process, input=True)
-        average, missing_udf_count = calculate_average_size(all_artifacts=artifacts)
+        artifacts: List[Artifact] = get_artifacts(process=process, input=True)
+        average: float = calculate_average_size(all_artifacts=artifacts)
         set_average_and_qc(
             average=average,
-            missing_udf_count=missing_udf_count,
             lower_threshold=lower_threshold,
             upper_threshold=upper_threshold,
-            all_artifacts=artifacts
+            all_artifacts=artifacts,
         )
         message = "Average Size (bp) have been calculated and set for all samples."
         LOG.info(message)
