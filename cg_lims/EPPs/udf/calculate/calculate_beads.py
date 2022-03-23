cb79fa0f15e5bbc51fe4834d2ffd3e575916229a
mayabrandi <maya.brandi@scilifelab.se>
2022-03-23 15:01:02 +0100

    update calc wather (#218)(major)
    ### Changed
    - calculate water script
    - calculate beads script
    
    Only in use in twist Workflow. Have been tested fro twist version 7.

--- cg_lims/EPPs/udf/calculate/calculate_beads.py
+++ cg_lims/EPPs/udf/calculate/calculate_beads.py
@@ -5,39 +5,21 @@ from typing import List
 
 import click
 from genologics.entities import Artifact
-
 from cg_lims.exceptions import LimsError, MissingUDFsError
 from cg_lims.get.artifacts import get_artifacts
 
 LOG = logging.getLogger(__name__)
 
-SAMPLE_VOLUME_BOUNDARY = 50.0
-ELUTION_VOLUME = 40.0
-ELUTION_VOLUME_FACTOR = 0.8
-
 
 def calculate_elution_volume(sample_volume: float) -> float:
-    """Calculates the elution volume based on the sample volume"""
+    """Calculates the elution volume. 80% of sample volume"""
 
-    return (
-        ELUTION_VOLUME
-        if sample_volume < SAMPLE_VOLUME_BOUNDARY
-        else ELUTION_VOLUME_FACTOR * sample_volume
-    )
+    return 0.8 * sample_volume
 
 
-def calculate_water_volume(sample_volume: float) -> float:
-    """Calculates the H20 volume based on the sample volume"""
-    return (
-        SAMPLE_VOLUME_BOUNDARY - sample_volume
-        if sample_volume < SAMPLE_VOLUME_BOUNDARY
-        else 0.0
-    )
-
-
-def calculate_beads_volume(sample_volume: float, h2o_volume: float) -> float:
-    """Calculates the bead volume bases on sample volume and H2O volume"""
-    return 2 * (sample_volume + h2o_volume)
+def calculate_beads_volume(sample_volume: float) -> float:
+    """Calculates the bead volume. Double of sample volume."""
+    return 2 * sample_volume
 
 
 def calculate_volumes(artifacts: List[Artifact]):
@@ -50,12 +32,8 @@ def calculate_volumes(artifacts: List[Artifact]):
         if sample_volume is None:
             missing_udfs += 1
             continue
-        h2o_volume = calculate_water_volume(sample_volume)
-        elution_volume = calculate_elution_volume(sample_volume)
-        beads_volume = calculate_beads_volume(sample_volume, h2o_volume)
-        artifact.udf["Volume Elution (ul)"] = elution_volume
-        artifact.udf["Volume H2O (ul)"] = h2o_volume
-        artifact.udf["Volume Beads (ul)"] = beads_volume
+        artifact.udf["Volume Elution (ul)"] = calculate_elution_volume(sample_volume)
+        artifact.udf["Volume Beads (ul)"] = calculate_beads_volume(sample_volume)
         artifact.put()
 
     if missing_udfs:
--- cg_lims/EPPs/udf/calculate/calculate_water.py
+++ cg_lims/EPPs/udf/calculate/calculate_water.py
@@ -1,55 +1,68 @@
+"""CLI module for calculating beads volumes"""
 import logging
 import sys
 from typing import List
 
 import click
-from genologics.entities import Artifact, Process
+from genologics.entities import Artifact
 
+from cg_lims import options
 from cg_lims.exceptions import LimsError, MissingUDFsError
 from cg_lims.get.artifacts import get_artifacts
 
 LOG = logging.getLogger(__name__)
 
 
-def calculate_volumes(artifacts: List[Artifact], process: Process):
-    """Calculates volumes of H2O for diluting samples. The total volume differ depending on
-    type of sample. It is given by the process udf 'Total Volume (ul)'."""
+def calculate_water_volume(sample_volume: float, sample_volume_limit: float) -> float:
+    """Calculates the H20 volume based on the sample volume"""
+    return sample_volume_limit - sample_volume if sample_volume < sample_volume_limit else 0.0
 
-    max_volume = process.udf.get("Total Volume (ul)")
-    if not max_volume:
-        raise MissingUDFsError("Process udf missing: Total Volume (ul)")
+
+def calculate_volumes(artifacts: List[Artifact], sample_volume_limit: float):
+    """Calculates water volume and total volume"""
 
     missing_udfs = 0
-
+    high_volume_warning = False
+    warning_message = ""
     for artifact in artifacts:
-        sample_volume = artifact.udf.get("Volume (ul)")
-
+        sample_volume: float = artifact.udf.get("Sample Volume (ul)")
         if sample_volume is None:
             missing_udfs += 1
             continue
-        volume_water = max_volume - sample_volume
-        if volume_water < 0:
-            volume_water = 0
-        artifact.udf["Volume H2O (ul)"] = volume_water
+        h2o_volume = calculate_water_volume(
+            sample_volume=sample_volume, sample_volume_limit=sample_volume_limit
+        )
+        total_volume = h2o_volume + sample_volume
+        if total_volume > 100:
+            high_volume_warning = True
+        artifact.udf["Volume H2O (ul)"] = h2o_volume
+        artifact.udf["Total Volume (uL)"] = total_volume
         artifact.put()
 
     if missing_udfs:
-        raise MissingUDFsError(f"Udf missing for {missing_udfs} samples")
+        warning_message += (
+            f'Udf "Sample Volume (ul)" missing for {missing_udfs} out of {len(artifacts)} samples '
+        )
+    if high_volume_warning:
+        warning_message += "Total volume higher than 100 ul for some samples!"
+    if warning_message:
+        raise MissingUDFsError(message=warning_message)
 
 
 @click.command()
+@options.sample_volume_limit()
 @click.pass_context
-def volume_water(ctx):
-    """Calculates water volume for samples."""
+def volume_water(context: click.Context, sample_volume_limit: float):
+    """Water volume calculation."""
 
-    LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
+    LOG.info(f"Running {context.command_path} with params: {context.params}")
 
-    process = ctx.obj["process"]
+    process = context.obj["process"]
 
     try:
-        artifacts = get_artifacts(process=process, input=False)
-        calculate_volumes(artifacts=artifacts, process=process)
-        message = "Volume H2O have been calculated for all samples."
+        artifacts: List[Artifact] = get_artifacts(process=process, input=False)
+        calculate_volumes(artifacts=artifacts, sample_volume_limit=sample_volume_limit)
+        message = "Beads volumes have been calculated."
         LOG.info(message)
         click.echo(message)
     except LimsError as e:
