b13ecef0f8567b0d798db7cd6873e06f832b67a7
RasmusBurge-CG <rasmus.burge@scilifelab.se>
2022-03-28 14:12:40 +0200

    Working on it

--- cg_lims/EPPs/udf/copy/base.py
+++ cg_lims/EPPs/udf/copy/base.py
@@ -10,6 +10,7 @@ from cg_lims.EPPs.udf.copy.reads_to_sequence import reads_to_sequence
 from cg_lims.EPPs.udf.copy.artifact_to_artifact import artifact_to_artifact
 from cg_lims.EPPs.udf.copy.process_to_artifact import process_to_artifact
 from cg_lims.EPPs.udf.copy.qc_to_sample import qc_to_sample
+from cg_lims.EPPs.udf.copy.orgwell_to_sample import orgwell_to_sample
 
 @click.group(invoke_without_command=True)
 @click.pass_context
@@ -24,3 +25,4 @@ copy.add_command(reads_to_sequence)
 copy.add_command(artifact_to_artifact)
 copy.add_command(process_to_artifact)
 copy.add_command(qc_to_sample)
+copy.add_command(orgwell_to_sample)
