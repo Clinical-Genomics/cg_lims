3e40399827bd9ec47619d0733f8a863e0e4b1938
mayabrandi <maya.brandi@scilifelab.se>
2022-04-01 12:47:42 +0200

    aggregation script (#247)(minor)
    ### Added
    aggregate qc script moved from clinical_epps

--- cg_lims/EPPs/udf/copy/base.py
+++ cg_lims/EPPs/udf/copy/base.py
@@ -10,12 +10,15 @@ from cg_lims.EPPs.udf.copy.reads_to_sequence import reads_to_sequence
 from cg_lims.EPPs.udf.copy.artifact_to_artifact import artifact_to_artifact
 from cg_lims.EPPs.udf.copy.process_to_artifact import process_to_artifact
 from cg_lims.EPPs.udf.copy.qc_to_sample import qc_to_sample
+from cg_lims.EPPs.udf.copy.aggregate_qc_flags_and_copy_fields import aggregate_qc_and_copy_fields
+
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def copy(ctx):
     """Main entry point of copy commands"""
     pass
+
 
 copy.add_command(artifact_to_sample)
 copy.add_command(sample_to_artifact)
@@ -24,3 +27,4 @@ copy.add_command(reads_to_sequence)
 copy.add_command(artifact_to_artifact)
 copy.add_command(process_to_artifact)
 copy.add_command(qc_to_sample)
+copy.add_command(aggregate_qc_and_copy_fields)
