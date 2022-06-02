a09c7874899f1042fe86676e9010c863e5d19141
Cecilia Svensson <37171914+cesve@users.noreply.github.com>
2022-06-02 08:17:49 +0200

    Removes_valid_input_amounts (#280)
    Changed
    Removed valid input amounts.
    Will allow udf 'Amount needed (ng)' to be anything other than zero.

--- cg_lims/EPPs/udf/calculate/calculate_water_volume_rna.py
+++ cg_lims/EPPs/udf/calculate/calculate_water_volume_rna.py
@@ -14,7 +14,6 @@ from cg_lims.get.artifacts import get_artifacts
 LOG = logging.getLogger(__name__)
 
 TOTAL_VOLUME = 25
-VALID_AMOUNTS_NEEDED = [200, 300, 400, 500]
 
 
 def calculate_sample_and_water_volumes(artifacts: List[Artifact]):
@@ -23,17 +22,12 @@ def calculate_sample_and_water_volumes(artifacts: List[Artifact]):
     for artifact in artifacts:
         concentration = artifact.udf.get("Concentration")
         amount_needed = artifact.udf.get("Amount needed (ng)")
-        if concentration is None:
+        if concentration is None or amount_needed is None:
             LOG.error(
-                f"Sample {artifact.samples[0].name} is missing udf 'Concentration'."
+                f"Sample {artifact.samples[0].name} is missing udf 'Concentration' or 'Amount needed'."
             )
             missing_udfs += 1
             continue
-        if amount_needed not in VALID_AMOUNTS_NEEDED:
-            raise InvalidValueError(
-                f"'Amount needed (ng)' missing or incorrect for one or more samples. Value can "
-                f"only be {', '.join(map(str, VALID_AMOUNTS_NEEDED))}."
-            )
         sample_volume = amount_needed / concentration
         artifact.udf["Sample Volume (ul)"] = sample_volume
         artifact.udf["Volume H2O (ul)"] = TOTAL_VOLUME - sample_volume
@@ -42,8 +36,9 @@ def calculate_sample_and_water_volumes(artifacts: List[Artifact]):
     if missing_udfs:
         raise MissingUDFsError(
             f"Could not apply calculation for {missing_udfs} out of {len(artifacts)} sample(s): "
-            f"'Concentration' is missing!"
+            f"'Concentration' or 'Amount needed (ng)' is missing!"
         )
+
 
 
 @click.command()
