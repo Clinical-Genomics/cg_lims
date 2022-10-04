ac932811abefabe9a72ed2ea0061556e12dc4ad3
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2022-10-04 14:51:59 +0200

    make the barcodes shorter and sort them (#324)
    ### Added
    - Sorting function
    
    ### Changed
    
    ### Fixed
    - Removed ACC from barcodes
    
    **Steps to consider while deploying**
    - Configuration changes:
    - Documentation updates:
    - Inform users by email:
    
    ### Review:
    - [x] Code approved by KS
    - [x] Tests executed on stage by:  RB
    - [x] "Merge and deploy" approved by RB
    
    This [version](https://semver.org/) is a:
    - [ ] **MAJOR** - when you make incompatible API changes
    - [ ] **MINOR** - when you add functionality in a backwards compatible manner
    - [x] **PATCH** - when you make backwards compatible bug fixes or documentation/instructions

--- cg_lims/EPPs/files/barcode_tubes.py
+++ cg_lims/EPPs/files/barcode_tubes.py
@@ -1,24 +1,23 @@
+import re
 import logging
 import sys
 import click
+import pandas as pd
 
 from pathlib import Path
 from typing import List
 from genologics.lims import Artifact
 from cg_lims import options
 from cg_lims.exceptions import LimsError, InvalidValueError, MissingValueError
-from cg_lims.files.manage_csv_files import build_csv
 from cg_lims.get.artifacts import get_artifacts
+from cg_lims.get.fields import get_barcode
+
 
 LOG = logging.getLogger(__name__)
 
-HEADERS = [
-    "Barcodes"
-]
-
-
 def get_data_and_write(
-    artifacts: List[Artifact], file: str,
+    artifacts: List[Artifact], 
+    file: str,
 ):
     """Making a barcode csv file with sample names as barcode."""
 
@@ -28,25 +27,32 @@ def get_data_and_write(
 
     for artifact in artifacts:
         try:
-            barcode = str(artifact.samples[0].id)
-            container_type = str(artifact.container.type.name)
+            barcode = get_barcode(artifact)
+            art_container_type = artifact.container.type.name
 
-            if container_type.lower() != "tube":
+            if art_container_type.lower() != "tube":
 
                 LOG.info(
-                    f"Sample {barcode} does not have container type \"Tube\" therefore excluded"
+                    f"Sample {barcode} does not have container type \"Tube\" therefore excluded."
                 )
                 continue
-
-            file_rows.append([barcode])
+            
+            
+            #Function to get project nr and sample id
+            f=lambda x: re.findall(r'\d+',x)
+            
+            file_rows.append([barcode, f(barcode)[0], int(f(barcode)[1])])
+            #                           ^^^^^          ^^^^^^
+            #  e.g.     ACC10001A1 =    10001             1 
+        
         except:
             unexpected_container_type = + 1
-            failed_samples.append(str(artifact.samples[0].id))
+            failed_samples.append(artifact.samples[0].id)
 
     if unexpected_container_type:
         failed_message = " ".join(map(str, failed_samples))
         raise InvalidValueError(
-            f"The following samples are missing a container: {failed_message}"
+            f"The following samples are missing a container: {failed_message} of type \"Tube\"."
         )
 
     if file_rows == []:
@@ -54,20 +60,36 @@ def get_data_and_write(
             f"Missing samples with container type \"Tube\"."
         )
 
-    build_csv(file=Path(file), rows=file_rows, headers=HEADERS)
+    # Sort and create csv file.
+    unfiltered_df = pd.DataFrame(
+        data=file_rows, 
+        index = None, 
+        columns = [
+            "Barcode",
+            "Project",
+            "id",
+        ]
+        ).sort_values(by=["Project","id"])
+    
+    # Filter off unwanted columns
+    out = unfiltered_df["Barcode"]
+    out.to_csv(Path(file), index=False)
+    
+
 
 
 @click.command()
 @options.file_placeholder(help="Barcode Tubes")
+@options.input()
 @click.pass_context
 def make_barcode_csv(
-    ctx: click.Context, file: str
+    ctx: click.Context, file: str, input: bool,
 ):
     """Script to make barcode for tubes"""
 
     LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
     process = ctx.obj["process"]
-    artifacts = get_artifacts(process=process, input=True)
+    artifacts = get_artifacts(process=process, input=input)
     try:
         get_data_and_write(
             artifacts=artifacts,
