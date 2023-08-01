0519b4c0eb57d89df54958b6febdbec3a1a4c62b
seallard <sebastian.allard@scilifelab.se>
2023-08-01 10:01:06 +0200

    Fix comments

--- tests/test_token_manager.py
+++ tests/test_token_manager.py
@@ -1,14 +1,13 @@
 import pytest
+from pytest_mock import MockFixture
 import time
 from cg_lims.token_manager import TokenManager
 
 
 class TestTokenManager:
     @pytest.fixture(autouse=True)
-    def mock_dependencies(self, mocker):
-        self.mock_encode = mocker.patch(
-            "cg_lims.token_manager.jwt.encode", return_value=b"test_token"
-        )
+    def mock_dependencies(self, mocker: MockFixture):
+        self.mock_encode = mocker.patch("cg_lims.token_manager.jwt.encode", return_value=b"test_token")
         self.mock_from_service_account_file = mocker.patch(
             "cg_lims.token_manager.RSASigner.from_service_account_file",
             return_value=mocker.MagicMock(),
@@ -19,7 +18,7 @@ class TestTokenManager:
         # WHEN the token property is accessed
         actual_token = token_manager.token
 
-        # THEN a new token should be generated and jwt.encode should be called
+        # THEN a new token should be generated
         assert actual_token == "test_token"
 
     def test_token_property_existing_token_valid(self, token_manager: TokenManager):
@@ -30,7 +29,7 @@ class TestTokenManager:
         # WHEN the token property is accessed
         actual_token = token_manager.token
 
-        # THEN the existing token should be returned and jwt.encode should not be called
+        # THEN the existing token should be returned
         assert actual_token == "existing_token"
 
     def test_token_property_existing_token_expired(self, token_manager: TokenManager):
