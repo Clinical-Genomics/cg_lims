3930c99d06e674bcd8723554bd50dcd6f641340c
seallard <sebastian.allard@scilifelab.se>
2023-08-03 08:00:10 +0200

    Fix test doc strings

--- tests/test_token_manager.py
+++ tests/test_token_manager.py
@@ -14,44 +14,48 @@ class TestTokenManager:
         )
         yield
 
-    def test_token_property_new_token_needed(self, token_manager: TokenManager):
-        # WHEN the token property is accessed
-        actual_token = token_manager.get_token()
+    def test_get_token(self, token_manager: TokenManager):
+        # WHEN a token is requested
+        token = token_manager.get_token()
 
-        # THEN a new token should be generated
-        assert actual_token == "test_token"
+        # THEN a token should be returned
+        assert token == "test_token"
 
-    def test_token_property_existing_token_valid(self, token_manager: TokenManager):
+    def test_get_token_existing_token_valid(self, token_manager: TokenManager):
         # GIVEN an existing valid token
-        token_manager._token = "existing_token"
+        token_manager._token = "valid_token"
         token_manager._expiration = time.time() + 1000
 
-        # WHEN the token property is accessed
+        # WHEN a token is requested
         actual_token = token_manager.get_token()
 
         # THEN the existing token should be returned
-        assert actual_token == "existing_token"
+        assert actual_token == "valid_token"
 
-    def test_token_property_existing_token_expired(self, token_manager: TokenManager):
+    def test_get_token_existing_token_expired(self, token_manager: TokenManager):
         # GIVEN an expired token
         token_manager._token = "expired_token"
         token_manager._expiration = time.time() - 1000
 
-        # WHEN the token property is accessed
-        self.mock_encode.return_value = b"new_token"
+        # GIVEN the next token that will be generated
+        self.mock_encode.return_value = b"next_token"
+
+        # WHEN a token is requested
         actual_token = token_manager.get_token()
 
-        # THEN a new token should be generated
-        assert actual_token == "new_token"
+        # THEN the next token should be returned
+        assert actual_token == "next_token"
 
-    def test_token_property_existing_token_close_to_expiry(self, token_manager: TokenManager):
+    def test_get_token_existing_token_close_to_expiry(self, token_manager: TokenManager):
         # GIVEN an existing token close to expiry
         token_manager._token = "almost_expired_token"
         token_manager._expiration = time.time() + TOKEN_RENEW_DURATION_IN_SECONDS - 10
 
-        # WHEN the token property is accessed
-        self.mock_encode.return_value = b"new_token"
+        # GIVEN the next token that will be generated
+        self.mock_encode.return_value = b"next_token"
+        
+        # WHEN a token is requested
         actual_token = token_manager.get_token()
 
-        # THEN a new token should be generated
-        assert actual_token == "new_token"
+        # THEN the next token should be returned
+        assert actual_token == "next_token"
