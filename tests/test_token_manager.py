694628418749e056a0efa05b78dc8fa1437cefb3
seallard <sebastian.allard@scilifelab.se>
2023-08-04 10:38:17 +0200

    Add doc string

--- tests/test_token_manager.py
+++ tests/test_token_manager.py
@@ -10,6 +10,7 @@ from cg_lims.token_manager import TOKEN_RENEW_DURATION_IN_SECONDS, TokenManager
 class TestTokenManager:
     @pytest.fixture(autouse=True)
     def mock_dependencies(self, mocker: MockFixture):
+        """Mock dependencies for the TokenManager class."""
         self.mock_encode = mocker.patch(
             "cg_lims.token_manager.jwt.encode", return_value=b"test_token"
         )
