18a429bd32d75103610d3c2de812e3894c98a79f
seallard <sebastian.allard@scilifelab.se>
2023-08-08 15:01:00 +0200

    Add command to cli group

--- cg_lims/EPPs/qc/base.py
+++ cg_lims/EPPs/qc/base.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import click
-
+from cg_lims.EPPs.qc.sequencing_quality_control import sequencing_quality_control
 from cg_lims.EPPs.qc.set_qc_fail import set_qc_fail
+
 
 
 @click.group(invoke_without_command=True)
@@ -12,3 +13,4 @@ def qc(ctx):
 
 
 qc.add_command(set_qc_fail)
+qc.add_command(sequencing_quality_control)
