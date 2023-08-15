026a98d4e491b369ff9cd90726c187be08f970cf
Karl Svärd <karl.svard@scilifelab.se>
2023-08-15 15:15:15 +0200

    add logging to CLI command

--- cg_lims/EPPs/qc/sequencing_quality_control.py
+++ cg_lims/EPPs/qc/sequencing_quality_control.py
@@ -2,13 +2,21 @@ import sys
 
 import click
 
+import logging
+
 from cg_lims.EPPs.qc.sequencing_quality_checker import SequencingQualityChecker
 from cg_lims.EPPs.qc.sequencing_artifact_manager import SequencingArtifactManager
+
+LOG = logging.getLogger(__name__)
 
 
 @click.command()
 @click.pass_context
 def sequencing_quality_control(ctx):
+    """Sequencing quality control script for the BCL conversion step in LIMS."""
+
+    LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
+
     process = ctx.obj["process"]
     status_db_api = ctx.obj["status_db"]
     lims = ctx.obj["lims"]
@@ -23,6 +31,8 @@ def sequencing_quality_control(ctx):
     quality_summary: str = quality_checker.validate_sequencing_quality()
 
     if quality_checker.samples_failed_quality_control():
+        LOG.error(quality_summary)
         sys.exit(quality_summary)
 
-    print(quality_summary, file=sys.stderr)
+    LOG.info(quality_summary)
+    click.echo(quality_summary)
