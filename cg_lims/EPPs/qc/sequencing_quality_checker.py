02f94abb08da0eddd45cccbb37ed6223e6cd5aa2
seallard <sebastian.allard@scilifelab.se>
2023-08-18 15:21:35 +0200

    Patch metrics

--- cg_lims/EPPs/qc/sequencing_quality_checker.py
+++ cg_lims/EPPs/qc/sequencing_quality_checker.py
@@ -63,19 +63,19 @@ class SequencingQualityChecker:
             sample_id=metrics.sample_internal_id,
             lane=metrics.flow_cell_lane_number,
             reads=metrics.sample_total_reads_in_lane,
-            q30_score=metrics.sample_base_fraction_passing_q30 * 100,
+            q30_score=metrics.sample_base_percentage_passing_q30,
             passed_quality_control=passed_quality_control,
         )
 
     def _quality_control(self, metrics: SampleLaneSequencingMetrics) -> bool:
         return self._passes_quality_thresholds(
             reads=metrics.sample_total_reads_in_lane,
-            q30_score=metrics.sample_base_fraction_passing_q30,
+            q30_score=metrics.sample_base_percentage_passing_q30,
         )
 
     def _passes_quality_thresholds(self, q30_score: float, reads: int) -> bool:
         """Check if the provided metrics pass the minimum quality thresholds."""
-        passes_q30_threshold = q30_score * 100 >= self.q30_threshold
+        passes_q30_threshold = q30_score >= self.q30_threshold
         passes_read_threshold = reads >= self.READS_MIN_THRESHOLD
         return passes_q30_threshold and passes_read_threshold
 
