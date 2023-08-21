0e475c7eefab7e8bab63ef8aca26622bfec28521
Karl Svärd <karl.svard@scilifelab.se>
2023-08-21 11:40:12 +0200

    add new flow cell model for novaseq x

--- cg_lims/EPPs/arnold/flow_cell.py
+++ cg_lims/EPPs/arnold/flow_cell.py
@@ -9,14 +9,25 @@ from requests import Response
 from cg_lims.exceptions import LimsError
 from cg_lims.get.artifacts import OutputGenerationType, OutputType, get_output_artifacts
 
-from cg_lims.models.arnold.flow_cell import FlowCell, Lane
+from cg_lims.models.arnold.flow_cell import NovaSeq6000FlowCell, NovaSeqXFlowCell, Lane
 
 
 LOG = logging.getLogger(__name__)
 
 
-def build_flow_cell_document(process: Process, lanes: List[Artifact]) -> FlowCell:
-    flowcell = FlowCell(**dict(process.udf.items()), date=process.date_run)
+def build_novaseq_6000_document(process: Process, lanes: List[Artifact]) -> NovaSeq6000FlowCell:
+    flowcell = NovaSeq6000FlowCell(**dict(process.udf.items()), date=process.date_run)
+
+    for lane in lanes:
+        if not lane.location:
+            continue
+        flowcell.lanes.append(Lane(name=lane.name, **dict(lane.udf.items())))
+
+    return flowcell
+
+
+def build_novaseq_x_document(process: Process, lanes: List[Artifact]) -> NovaSeqXFlowCell:
+    flowcell = NovaSeqXFlowCell(**dict(process.udf.items()), date=process.date_run)
 
     for lane in lanes:
         if not lane.location:
@@ -29,7 +40,7 @@ def build_flow_cell_document(process: Process, lanes: List[Artifact]) -> FlowCel
 @click.command()
 @click.pass_context
 def flow_cell(ctx):
-    """Creating FlowCell documents from a run in the arnold flow_cell collection."""
+    """Creating flow cell documents from a run in the arnold flow_cell collection."""
 
     LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
     process: Process = ctx.obj["process"]
@@ -41,7 +52,9 @@ def flow_cell(ctx):
         lims=lims,
         output_type=OutputType.RESULT_FILE,
     )
-    flow_cell_document: FlowCell = build_flow_cell_document(process=process, lanes=lanes)
+    flow_cell_document: NovaSeq6000FlowCell = build_novaseq_6000_document(
+        process=process, lanes=lanes
+    )
     response: Response = requests.post(
         url=f"{arnold_host}/flow_cell",
         headers={"Content-Type": "application/json"},
