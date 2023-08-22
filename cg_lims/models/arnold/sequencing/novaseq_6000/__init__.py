4a14cc8790e2742d6c080541a5e695a14c47163a
Karl Svärd <60181709+Karl-Svard@users.noreply.github.com>
2023-08-22 14:19:33 +0200

    Update cg_lims/models/arnold/sequencing/novaseq_6000/__init__.py
    Co-authored-by: Amanda Raud <91953737+amandaraud@users.noreply.github.com>

--- cg_lims/models/arnold/sequencing/novaseq_6000/__init__.py
+++ cg_lims/models/arnold/sequencing/novaseq_6000/__init__.py
@@ -14,7 +14,7 @@ from cg_lims.models.arnold.base_step import BaseStep
 def build_novaseq_6000_step_documents(
     sample_id: str, process_id: str, lims: Lims
 ) -> List[BaseStep]:
-    """Building a Step Documents for a NovaSeq sequencing run."""
+    """Building a Step Documents for a NovaSeq 6000 sequencing run."""
 
     prep_id = f"{sample_id}_{process_id}"
     step_documents = [
