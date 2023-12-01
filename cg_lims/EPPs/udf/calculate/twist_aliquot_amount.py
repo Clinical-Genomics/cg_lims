a64c52b0278d2f3e456c584ec7e9002031404f08
Karl Svärd <karl.svard@scilifelab.se>
2023-12-01 10:40:59 +0100

    patch twist aliquot amount

--- cg_lims/EPPs/udf/calculate/twist_aliquot_amount.py
+++ cg_lims/EPPs/udf/calculate/twist_aliquot_amount.py
@@ -7,6 +7,7 @@ from genologics.entities import Artifact
 
 from cg_lims.exceptions import LimsError, MissingUDFsError
 from cg_lims.get.artifacts import get_artifacts
+from cg_lims.get.samples import get_one_sample_from_artifact
 
 LOG = logging.getLogger(__name__)
 
@@ -16,21 +17,31 @@ LOG = logging.getLogger(__name__)
 MAXIMUM_SAMPLE_AMOUNT = 250
 
 
+def get_maximum_amount(artifact: Artifact) -> float:
+    """Return the maximum allowed input amount of an artifact."""
+    sample = get_one_sample_from_artifact(artifact=artifact)
+    maximum_amount = sample.udf.get("Maximum input amount (ng)")
+    if maximum_amount:
+        return maximum_amount
+    return MAXIMUM_SAMPLE_AMOUNT
+
+
 def set_amount_needed(artifacts: List[Artifact]):
     """The maximum amount taken into the prep is MAXIMUM_SAMPLE_AMOUNT.
     Any amount below this can be used in the prep if the total amount is limited."""
 
     missing_udfs = 0
-    for art in artifacts:
-        amount = art.udf.get("Amount (ng)")
+    for artifact in artifacts:
+        amount = artifact.udf.get("Amount (ng)")
+        maximum_amount = get_maximum_amount(artifact=artifact)
         if not amount:
             missing_udfs += 1
             continue
-        if amount >= MAXIMUM_SAMPLE_AMOUNT:
-            art.udf["Amount needed (ng)"] = MAXIMUM_SAMPLE_AMOUNT
+        if amount >= maximum_amount:
+            artifact.udf["Amount needed (ng)"] = maximum_amount
         else:
-            art.udf["Amount needed (ng)"] = amount
-        art.put()
+            artifact.udf["Amount needed (ng)"] = amount
+        artifact.put()
 
     if missing_udfs:
         raise MissingUDFsError(f"Udf missing for {missing_udfs} samples")
