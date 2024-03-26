914a42c052d2393eb849b2942ff11bd297ed4555
Karl Svärd <karl.svard@scilifelab.se>
2024-03-26 14:13:01 +0100

    add EPP for copying container values to artifact UDFs

--- cg_lims/options.py
+++ cg_lims/options.py
@@ -390,7 +390,7 @@ def add_volume(
 ) -> click.option:
     return click.option("--add-volume", required=False, default=0, help=help)
 
-  
+
 def amount_fmol_udf(
     help: str = "String of UDF used to get amount (fmol)",
 ) -> click.option:
@@ -409,3 +409,11 @@ def total_volume_udf(
     help: str = "String of process UDF used to get the total volume",
 ) -> click.option:
     return click.option("--total-volume-udf", required=False, help=help)
+
+
+def well_udf(help: str = "UDF name for artifact well.") -> click.option:
+    return click.option("--well-udf", required=False, default=None, help=help)
+
+
+def container_name_udf(help: str = "UDF name for container name.") -> click.option:
+    return click.option("--container-name-udf", required=False, default=None, help=help)
