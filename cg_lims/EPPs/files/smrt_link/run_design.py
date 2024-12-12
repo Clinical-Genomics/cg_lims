a93b5bae33a9df13429dcc4a99e68df630688e9b
Karl Svärd <60181709+Karl-Svard@users.noreply.github.com>
2024-12-12 15:35:14 +0100

    SMRT Link EPPs (#563)
    ### Added
    - New EPP for creating Sample Setup CSVs
    - New EPP for creating pooling calculation CSVs
    - New EPP for calculating Revio pooling volumes
    - New EPP for automatically setting Revio sequencing options
    - New models and classes for SMRT Link CSV creation

---
+++ cg_lims/EPPs/files/smrt_link/run_design.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+
+import click
+from cg_lims import options
+from cg_lims.EPPs.files.smrt_link.models import RevioRun
+from cg_lims.exceptions import LimsError
+from genologics.entities import Process
+
+LOG = logging.getLogger(__name__)
+
+
+@click.command()
+@options.file_placeholder()
+@click.pass_context
+def create_smrtlink_run_design(ctx, file: str):
+    """Create a run design .csv file for SMRT Link import."""
+    LOG.info(f"Running {ctx.command_path} with params: {ctx.params}")
+
+    process: Process = ctx.obj["process"]
+
+    try:
+        revio_run: RevioRun = RevioRun(process=process)
+        csv_string: str = revio_run.create_csv()
+        with open(f"{file}_run_design.csv", "w") as file:
+            file.write(csv_string)
+        click.echo("The run design CSV was successfully generated.")
+    except LimsError as e:
+        sys.exit(e.message)
