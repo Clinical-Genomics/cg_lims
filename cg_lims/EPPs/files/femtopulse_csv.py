aa06f8ef6d3cfd16c44f13714c987de97a2099df
idalindegaard <ida.lindegaard@scilifelab.se>
2024-12-16 14:34:53 +0100

    updated error handling again

--- cg_lims/EPPs/files/femtopulse_csv.py
+++ cg_lims/EPPs/files/femtopulse_csv.py
@@ -60,11 +60,15 @@ def get_data_and_write(
                                 "error": "Position is not possible for the run"})
 
     if failed_samples:
-        error_message: str = "\n".join(
-            f"Sample {sample['artifact_name']} in position {sample['parsed_well']}: {sample['error']}"
-        for sample in failed_samples
-        )
-        raise InvalidValueError(f"Errors found:\n{error_message}")
+        error_index: int = 0
+        for sample in failed_samples:
+            error_message: str = f"Sample {sample['artifact_name']} in position {sample['parsed_well']}: {sample['error']}"
+            if error_index < 1:
+                all_errors: str = error_message
+                error_index =+ 1
+            else:
+                all_errors = all_errors + '\n' + error_message
+        raise InvalidValueError(f"Errors found:\n{all_errors}")
 
     # The ladder will always be in well A12
     SAMPLE_NAMES[-1] = "ladder"
