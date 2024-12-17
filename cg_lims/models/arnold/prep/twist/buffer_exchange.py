6e6b00e511f328dc7021c0ece475239e51cd445f
idalindegaard <ida.lindegaard@scilifelab.se>
2024-12-17 10:20:44 +0100

    new elution buffer udfs in arnold twist

--- cg_lims/models/arnold/prep/twist/buffer_exchange.py
+++ cg_lims/models/arnold/prep/twist/buffer_exchange.py
@@ -10,8 +10,14 @@ class ArtifactUDFs(BaseModel):
     concentration: Optional[float] = Field(None, alias="Concentration")
 
 
+class ProcessUDFs(BaseModel):
+    lot_nr_elution_buffer_bex: Optional[str] = Field(None, alias="Lot no: Elution Buffer")
+    )
+
+
 class ArnoldStep(BaseStep):
     artifact_udfs: ArtifactUDFs
+    process_udfs: ProcessUDFs
 
     class Config:
         allow_population_by_field_name = True
@@ -31,6 +37,7 @@ def get_buffer_exchange_twist(lims: Lims, sample_id: str, prep_id: str) -> Optio
     return ArnoldStep(
         **analyte.base_fields(),
         artifact_udfs=ArtifactUDFs(**analyte.artifact_udfs()),
+        process_udfs=ProcessUDFs(**analyte.process_udfs()),
         sample_id=sample_id,
         prep_id=prep_id,
         step_type="buffer_exchange",
