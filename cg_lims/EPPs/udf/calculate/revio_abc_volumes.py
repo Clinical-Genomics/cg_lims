2e21c674bbd3e43c916d1accf83bf62d39fb06d5
Karl Svärd <karl.svard@scilifelab.se>
2025-01-23 16:05:01 +0100

    black and isort

--- cg_lims/EPPs/udf/calculate/revio_abc_volumes.py
+++ cg_lims/EPPs/udf/calculate/revio_abc_volumes.py
@@ -3,11 +3,10 @@ import sys
 from typing import List
 
 import click
-from genologics.entities import Artifact, Process
-
 from cg_lims import options
 from cg_lims.exceptions import LimsError, MissingValueError
 from cg_lims.get.artifacts import get_artifacts
+from genologics.entities import Artifact, Process
 
 LOG = logging.getLogger(__name__)
 
@@ -38,9 +37,7 @@ def set_polymerase_dilution_mix_per_sample(
     polymerase_dilution_volume: float = (
         float(polymerase_dilution_mix_ratio) * artifact.udf[volume_udf]
     )
-    artifact.udf["Volume Polymerase Dilution Mix (ul)"] = round(
-        polymerase_dilution_volume, 2
-    )
+    artifact.udf["Volume Polymerase Dilution Mix (ul)"] = round(polymerase_dilution_volume, 2)
     artifact.put()
 
 
@@ -54,15 +51,13 @@ def calculate_and_set_total_ABC_volumes(
     polymerase_dilution_mix_ratio: str,
     sequencing_polymerase_ratio: str,
 ) -> None:
-    """Calculate and set the total master mix and reagent volumes needed for all samples in the step. 
+    """Calculate and set the total master mix and reagent volumes needed for all samples in the step.
     Adding some excess specified in the cli command."""
     total_sample_volume: float = calculate_total_sample_volume(
         artifacts=artifacts, volume_udf=volume_udf
     )
 
-    process.udf["Total Annealing Mix Volume (ul)"] = round(
-        float(factor) * total_sample_volume, 1
-    )
+    process.udf["Total Annealing Mix Volume (ul)"] = round(float(factor) * total_sample_volume, 1)
     process.udf["Annealing Buffer Volume (ul)"] = round(
         float(factor) * float(annealing_reagent_ratio) * total_sample_volume, 1
     )
@@ -109,8 +104,7 @@ def revio_abc_volumes(
 
         for artifact in artifacts:
             if artifact.udf.get(volume_udf) == None:
-                error_message = (
-                "Missing a value for one or more sample volumes.")
+                error_message = "Missing a value for one or more sample volumes."
                 LOG.error(error_message)
                 raise MissingValueError(error_message)
             set_annealing_mix_per_sample(artifact=artifact, volume_udf=volume_udf)
@@ -130,9 +124,7 @@ def revio_abc_volumes(
             polymerase_dilution_mix_ratio=polymerase_dilution_mix_ratio,
             sequencing_polymerase_ratio=sequencing_polymerase_ratio,
         )
-        message: str = (
-            "ABC volumes have been calculated for all artifacts and process UDFs!"
-        )
+        message: str = "ABC volumes have been calculated for all artifacts and process UDFs!"
         LOG.info(message)
         click.echo(message)
     except LimsError as e:
