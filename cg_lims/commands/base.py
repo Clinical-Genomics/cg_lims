5000fdf71d0ff2b81c935838527a77105c9d3131
Karl Svärd <60181709+Karl-Svard@users.noreply.github.com>
2025-02-06 12:16:59 +0100

    Add SMRT Link API client and EPP for fetching run information (#586)(major)
    ### Added
    - Added a client for working with the SMRT Link API
    - Added a new EPP for fetching run information from SMRT Link
    - Added support for the updated LIMS config
    
    ### Changed
    - Refactored the location of modules used for setting up the API connection to StatusDB (cg)

--- cg_lims/commands/base.py
+++ cg_lims/commands/base.py
@@ -4,12 +4,13 @@ from typing import Dict
 import click
 import yaml
 from cg_lims import options
+from cg_lims.clients.cg.status_db_api import StatusDBAPI
+from cg_lims.clients.cg.token_manager import TokenManager
+from cg_lims.clients.smrt_link.models import SmrtLinkConfig
 
 # commands
 from cg_lims.EPPs import epps
 from cg_lims.scripts.base import scripts
-from cg_lims.status_db_api import StatusDBAPI
-from cg_lims.token_manager import TokenManager
 from genologics.lims import Lims
 
 
@@ -34,9 +35,17 @@ def cli(ctx, config):
     cg_url: str = config_data.get("CG_URL")
     status_db: StatusDBAPI = StatusDBAPI(base_url=cg_url, token_manager=token_manager)
 
+    smrt_link_host: str = config_data.get("SMRT_LINK_HOST")
+    smrt_link_user: str = config_data.get("SMRT_LINK_USER")
+    smrt_link_pass: str = config_data.get("SMRT_LINK_PASSWORD")
+    smrt_link_config: SmrtLinkConfig = SmrtLinkConfig(
+        host=smrt_link_host, username=smrt_link_user, password=smrt_link_pass
+    )
+
     ctx.ensure_object(dict)
     ctx.obj["lims"] = lims
     ctx.obj["status_db"] = status_db
+    ctx.obj["smrt_link"] = smrt_link_config
     ctx.obj["arnold_host"] = config_data.get("ARNOLD_HOST")
     ctx.obj["atlas_host"] = config_data.get("ATLAS_HOST")
     ctx.obj["db_uri"] = config_data.get("DB_URI")
